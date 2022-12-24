474037adbf37886f124b82b1f82f0deac435c405
Orlando2911 <orlando@pricode.io>
2022-12-23 22:13:16 -0500

    Agregar archivos de proyecto.

---
+++ Models/Costumer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TCrud.Models;
+
+public partial class Costumer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public DateTime BorthDate { get; set; }
+}
---
+++ Models/TamtestContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TCrud.Models;
+
+public partial class TamtestContext : DbContext
+{
+    public TamtestContext()
+    {
+    }
+
+    public TamtestContext(DbContextOptions<TamtestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Costumer> Costumers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Costumer>(entity =>
+        {
+            entity.ToTable("Costumer");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.BorthDate)
+                .HasColumnType("datetime")
+                .HasColumnName("borth_date");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("gender");
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("lastname");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
